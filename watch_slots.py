441fb4e0ce1cc681f3d0d3a3185eb91fb2c5ea55
Matias Senger <m.senger@hotmail.com>
2022-09-22 15:56:31 +0200

    Aesthetic

--- watch_slots.py
+++ watch_slots.py
@@ -8,6 +8,6 @@ while True:
 	bias_voltages = [s.measure_bias_voltage(slot_number) for slot_number in s.get_slots_configuration_df().index]
 	bias_currents = [s.measure_bias_current(slot_number) for slot_number in s.get_slots_configuration_df().index]
 	devices_namces = [s.get_name_of_device_in_slot_number(slot_number) for slot_number in s.get_slots_configuration_df().index]
-	print('---')
+	print('\n---')
 	for sn,V,I,device_name in zip(s.get_slots_configuration_df().index, bias_voltages, bias_currents, devices_namces):
-		print(f'{sn}: {repr(device_name)} {V:.2f} V | {I*1e6:.2f} µA')
+		print(f'\n{sn}: {repr(device_name)} {V:.2f} V | {I*1e6:.2f} µA', end='')
