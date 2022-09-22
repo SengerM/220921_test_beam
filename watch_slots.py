529b5fbb21f58a6745ac9e08796c027f76d5b7d3
Matias Senger <m.senger@hotmail.com>
2022-09-22 15:55:03 +0200

    Minor stuff

--- watch_slots.py
+++ watch_slots.py
@@ -7,6 +7,7 @@ while True:
 	time.sleep(1)
 	bias_voltages = [s.measure_bias_voltage(slot_number) for slot_number in s.get_slots_configuration_df().index]
 	bias_currents = [s.measure_bias_current(slot_number) for slot_number in s.get_slots_configuration_df().index]
+	devices_namces = [s.get_name_of_device_in_slot_number(slot_number) for slot_number in s.get_slots_configuration_df().index]
 	print('---')
-	for sn,V,I in zip(s.get_slots_configuration_df().index, bias_voltages, bias_currents):
-		print(f'{sn}: {V:.2f} V | {I*1e6:.2f} µA')
+	for sn,V,I,device_name in zip(s.get_slots_configuration_df().index, bias_voltages, bias_currents, devices_namces):
+		print(f'{sn}: {repr(device_name)} {V:.2f} V | {I*1e6:.2f} µA')
