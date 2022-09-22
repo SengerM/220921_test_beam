b3b50177aa99ea5e3fbf7634c4c65b7bdbcfa074
Matias Senger <m.senger@hotmail.com>
2022-09-22 15:15:41 +0200

    Implemented the acquisition of extra data

--- acquire_test_beam.py
+++ acquire_test_beam.py
@@ -5,6 +5,28 @@ import datetime
 import time
 from TheSetup import connect_me_with_the_setup
 from huge_dataframe.SQLiteDataFrame import SQLiteDataFrameDumper # https://github.com/SengerM/huge_dataframe
+import datetime
+
+def trigger_and_measure_dut_stuff(the_setup, name_to_access_to_the_setup:str, slots_numbers:list)->pandas.DataFrame:
+	elapsed_seconds = 9999
+	while elapsed_seconds > 5: # Because of multiple threads locking the different elements of the_setup, it can happen that this gets blocked for a long time. Thus, the measured data will no longer belong to a single point in time as we expect...:
+		the_setup.wait_for_trigger(who=name_to_access_to_the_setup)
+		trigger_time = time.time()
+		stuff = []
+		for slot_number in slots_numbers:
+			measured_stuff = {
+				'Bias voltage (V)': the_setup.measure_bias_voltage(slot_number),
+				'Bias current (A)': the_setup.measure_bias_current(slot_number),
+				'device_name': the_setup.get_name_of_device_in_slot_number(slot_number),
+				'slot_number': slot_number,
+				'When': datetime.datetime.now()
+			}
+			measured_stuff = pandas.DataFrame(measured_stuff, index=[0])
+			stuff.append(measured_stuff)
+		elapsed_seconds = trigger_time - time.time()
+	return pandas.concat(stuff)
+
+INDEX_COLUMNS = ['n_trigger','slot_number']
 
 def acquire_test_beam_data(bureaucrat:RunBureaucrat, the_setup, name_to_access_to_the_setup:str, n_triggers:int, slots_numbers:list, silent:bool=True):
 	with bureaucrat.handle_task('acquire_test_beam_data') as employee, \
@@ -13,16 +35,29 @@ def acquire_test_beam_data(bureaucrat:RunBureaucrat, the_setup, name_to_access_t
 			employee.path_to_directory_of_my_task/'waveforms.sqlite',
 			dump_after_n_appends = 1111,
 			dump_after_seconds = 11,
-		) as waveforms_dumper \
+		) as waveforms_dumper, \
+		SQLiteDataFrameDumper(
+			employee.path_to_directory_of_my_task/'extra_stuff.sqlite',
+			dump_after_n_appends = 1111,
+			dump_after_seconds = 11,
+		) as extra_stuff_dumper \
 	:
 		with open(employee.path_to_directory_of_my_task/'setup_description.txt', 'w') as ofile:
 			print(the_setup.get_description(), file=ofile)
 		n_trigger = -1
 		while n_trigger < n_triggers:
+			n_trigger += 1
+			
 			if not silent:
 				print(f'Waiting for trigger in the oscilloscope...')
-			the_setup.wait_for_trigger(name_to_access_to_the_setup)
-			n_trigger += 1
+			measured_stuff = trigger_and_measure_dut_stuff(
+				the_setup = the_setup,
+				name_to_access_to_the_setup = name_to_access_to_the_setup,
+				slots_numbers = slots_numbers,
+			)
+			measured_stuff['n_trigger'] = n_trigger
+			extra_stuff_dumper.append(measured_stuff.set_index(INDEX_COLUMNS))
+			
 			if not silent:
 				print(f'Acquiring n_trigger {n_trigger} out of {n_triggers}...')
 			data = []
@@ -34,7 +69,7 @@ def acquire_test_beam_data(bureaucrat:RunBureaucrat, the_setup, name_to_access_t
 					data[i]['n_trigger'] = n_trigger + i
 					data[i]['slot_number'] = slot_number
 					data[i] = pandas.DataFrame(data[i])
-					waveforms_dumper.append(data[i].set_index(['n_trigger','slot_number']))
+					waveforms_dumper.append(data[i].set_index(INDEX_COLUMNS))
 			n_trigger = data[-1]['n_trigger'].max()
 
 if __name__=='__main__':
@@ -52,7 +87,7 @@ if __name__=='__main__':
 		acquire_test_beam_data(
 			bureaucrat = Mariano,
 			name_to_access_to_the_setup = NAME_TO_ACCESS_TO_THE_SETUP,
-			n_triggers = 9999,
+			n_triggers = 999,
 			the_setup = the_setup,
 			slots_numbers = SLOTS,
 			silent = False,
