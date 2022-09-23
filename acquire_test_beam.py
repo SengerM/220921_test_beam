213745a0ba667b2606846fc7389d56c10c832eaf
Matias Senger <m.senger@hotmail.com>
2022-09-23 02:15:00 +0200

    Implemented `acquire_test_beam_data_sweeping_bias_voltage`

--- acquire_test_beam.py
+++ acquire_test_beam.py
@@ -9,7 +9,8 @@ import datetime
 import threading
 from parse_waveforms import parse_waveforms
 from progressreporting.TelegramProgressReporter import TelegramReporter # https://github.com/SengerM/progressreporting
-from contextlib import nullcontext
+from contextlib import nullcontext, ExitStack
+import my_telegram_bots
 
 def trigger_and_measure_dut_stuff(the_setup, name_to_access_to_the_setup:str, slots_numbers:list)->pandas.DataFrame:
 	elapsed_seconds = 9999
@@ -143,34 +144,75 @@ def acquire_and_parse(bureaucrat:RunBureaucrat, the_setup, name_to_access_to_the
 		
 		if delete_waveforms_file == True:
 			(Ernestino.path_to_directory_of_task('acquire_test_beam_data')/'waveforms.sqlite').unlink()
+
+def acquire_test_beam_data_sweeping_bias_voltage(bureaucrat:RunBureaucrat, the_setup, name_to_access_to_the_setup:str, n_triggers_per_voltage:int, slots_numbers:list, bias_voltages:dict, delete_waveforms_file:bool, reporter:TelegramReporter=None, silent:bool=True):
+	if set(slots_numbers) != set(bias_voltages.keys()):
+		raise ValueError(f'`bias_voltages` must be a dictionary whose keys are the same as the `slots_numbers`.')
+	if any([len(bias_voltages[k])!=len(bias_voltages[list(bias_voltages.keys())[0]]) for k in bias_voltages.keys()]):
+		raise ValueError(f'`bias_voltages` must be a dictionary of lists, all lists with the same lengths. If you dont want to sweep a voltage, just fill in a list with all identical values.')
+	if not silent:
+		print(f'Waiting to acquire control of hardware...')
+	
+	report_progress = reporter is not None
+	with ExitStack() as stack, \
+		bureaucrat.handle_task('acquire_test_beam_data_sweeping_bias_voltage') as employee, \
+		reporter.report_for_loop(len(bias_voltages[slots_numbers[0]]), f'{bureaucrat.run_name}') if report_progress else nullcontext() as reporter \
+	:
+		for mgr in [the_setup.hold_control_of_bias_for_slot_number(slot_number=sn, who=name_to_access_to_the_setup) for sn in slots_numbers]:
+			stack.enter_context(mgr)
+		if not silent:
+			print(f'Control of hardware acquired!')
+		for i_voltage in range(len(bias_voltages[slots_numbers[0]])):
+			for slot_number in slots_numbers:
+				if not silent:
+					print(f'Setting voltage {bias_voltages[slot_number][i_voltage]} to slot_number {slot_number}...')
+				the_setup.set_bias_voltage(
+					slot_number = slot_number,
+					volts = bias_voltages[slot_number][i_voltage],
+					who = name_to_access_to_the_setup,
+				)
+			acquire_and_parse(
+				bureaucrat = employee.create_subrun(f'{bureaucrat.run_name}_i_voltage_{i_voltage}'),
+				name_to_access_to_the_setup = NAME_TO_ACCESS_TO_THE_SETUP,
+				n_triggers = n_triggers_per_voltage,
+				the_setup = the_setup,
+				slots_numbers = slots_numbers,
+				delete_waveforms_file = delete_waveforms_file,
+				silent = silent,
+				reporter = TelegramReporter(
+					telegram_token = my_telegram_bots.robobot.token, 
+					telegram_chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
+				) if report_progress else None,
+			)
+			if report_progress:
+				reporter.update(1)
+	
 	
 if __name__=='__main__':
 	import os
 	from configuration_files.current_run import Alberto
 	from utils import create_a_timestamp
-	import my_telegram_bots
 	
-	SLOTS = [1,2,3,4]
 	NAME_TO_ACCESS_TO_THE_SETUP = f'acquire test bean PID: {os.getpid()}'
 	
 	the_setup = connect_me_with_the_setup()
 	
+	VOLTAGES = [222,200,180]
+	
 	with Alberto.handle_task('test_beam_data', drop_old_data=False) as employee:
 		Mariano = employee.create_subrun(create_a_timestamp() + '_' + input('Measurement name? ').replace(' ','_'))
-		# ~ acquire_test_beam_data(
-			# ~ bureaucrat = Mariano,
-			# ~ name_to_access_to_the_setup = NAME_TO_ACCESS_TO_THE_SETUP,
-			# ~ n_triggers = 5555,
-			# ~ the_setup = the_setup,
-			# ~ slots_numbers = SLOTS,
-			# ~ silent = False,
-		# ~ )
-		acquire_and_parse(
+		acquire_test_beam_data_sweeping_bias_voltage(
 			bureaucrat = Mariano,
 			the_setup = the_setup,
 			name_to_access_to_the_setup = NAME_TO_ACCESS_TO_THE_SETUP,
-			n_triggers = 9999,
+			n_triggers_per_voltage = 99,
 			slots_numbers = [1,2,3,4],
+			bias_voltages = {
+				1: [220]*len(VOLTAGES),
+				2: VOLTAGES,
+				3: [99]*len(VOLTAGES),
+				4: VOLTAGES,
+			},
 			delete_waveforms_file = False,
 			silent = False,
 			reporter = TelegramReporter(
