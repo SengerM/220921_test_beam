013f9899e12cdab28a087c7c3c8c93ee878b62d2
Matias Senger <m.senger@hotmail.com>
2022-09-23 12:02:43 +0200

    New setup configuration

--- configuration_files/current_run.py
+++ configuration_files/current_run.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 from the_bureaucrat.bureaucrats import RunBureaucrat # https://github.com/SengerM/the_bureaucrat
 
-Alberto = RunBureaucrat(Path.home()/Path('measurements_data/testing'))
+Alberto = RunBureaucrat(Path.home()/Path('measurements_data/20220923_2TIs_2MSs'))
 Alberto.create_run()
