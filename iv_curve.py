351540ead963f8f90f0a14c2eb16c178f64c8ab9
Matias Senger <m.senger@hotmail.com>
2022-09-23 11:54:42 +0200

    Added Telegram reporter to the IV curves

--- iv_curve.py
+++ iv_curve.py
@@ -10,8 +10,10 @@ import warnings
 import sys 
 sys.path.append(str(Path.home()/'scripts_and_codes/repos/robocold_beta_setup/analysis_scripts'))
 from plot_iv_curves import plot_IV_curves_all_together
+from progressreporting.TelegramProgressReporter import TelegramReporter # https://github.com/SengerM/progressreporting
+import my_telegram_bots
 
-def measure_iv_curve(bureaucrat:RunBureaucrat, voltages:list, slot_number:int, n_measurements_per_voltage:int, name_to_access_to_the_setup:str, current_compliance:float, silent=False)->Path:
+def measure_iv_curve(bureaucrat:RunBureaucrat, voltages:list, slot_number:int, n_measurements_per_voltage:int, name_to_access_to_the_setup:str, current_compliance:float, silent=False, reporter:TelegramReporter=None):
 	"""Measure an IV curve.
 	Parameters
 	----------
@@ -40,6 +42,7 @@ def measure_iv_curve(bureaucrat:RunBureaucrat, voltages:list, slot_number:int, n
 	John.create_run()
 	
 	the_setup = connect_me_with_the_setup()
+	report_progress = reporter is not None
 	
 	if not silent:
 		print('Waiting for acquiring control of the hardware.')
@@ -84,10 +87,12 @@ def measure_iv_curve(bureaucrat:RunBureaucrat, voltages:list, slot_number:int, n
 						measured_data_df.set_index(['n_voltage','n_measurement'], inplace=True)
 						measured_data_dumper.append(measured_data_df)
 						time.sleep(.5)
+						if report_progress:
+							reporter.update(1)
 	if not silent:
 		print(f'Finished measuring IV curve on slot {slot_number}...')
 
-def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict, current_compliances:dict, n_measurements_per_voltage:int, name_to_access_to_the_setup:str, silent:bool=False)->Path:
+def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict, current_compliances:dict, n_measurements_per_voltage:int, name_to_access_to_the_setup:str, silent:bool=False):
 	"""Measure the IV curve of multiple slots.
 	
 	Parameters
@@ -109,7 +114,7 @@ def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict,
 	"""
 	
 	class MeasureIVCurveThread(threading.Thread):
-		def __init__(self, bureaucrat:RunBureaucrat, slot_number:int, voltages_to_measure:list, n_measurements_per_voltage:int, name_to_access_to_the_setup:str, current_compliance:float, silent:bool):
+		def __init__(self, bureaucrat:RunBureaucrat, slot_number:int, voltages_to_measure:list, n_measurements_per_voltage:int, name_to_access_to_the_setup:str, current_compliance:float, silent:bool, reporter:TelegramReporter):
 			threading.Thread.__init__(self)
 			self.bureaucrat = bureaucrat
 			self.slot_number = slot_number
@@ -118,6 +123,7 @@ def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict,
 			self.n_measurements_per_voltage = n_measurements_per_voltage
 			self.current_compliance = current_compliance
 			self.silent = silent
+			self.reporter = reporter
 		def run(self):
 			measure_iv_curve(
 				bureaucrat = self.bureaucrat,
@@ -127,6 +133,7 @@ def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict,
 				n_measurements_per_voltage = self.n_measurements_per_voltage, 
 				current_compliance = self.current_compliance,
 				silent = self.silent,
+				reporter = self.reporter,
 			)
 	
 	Richard = bureaucrat
@@ -138,8 +145,13 @@ def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict,
 		raise ValueError(f'The keys of `voltages` and `current_compliances` do not coincide. They should specify the same slot numbers to measure.')
 	
 	the_setup = connect_me_with_the_setup()
-	
-	with Richard.handle_task('measure_iv_curves_on_multiple_slots') as measure_iv_curves_on_multiple_slots_task_handler:
+	reporter = TelegramReporter(
+		telegram_token = my_telegram_bots.robobot.token, 
+		telegram_chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
+	)
+	with Richard.handle_task('measure_iv_curves_on_multiple_slots') as measure_iv_curves_on_multiple_slots_task_handler, \
+		reporter.report_for_loop(sum([len(v) for _,v in voltages.items()])*n_measurements_per_voltage, bureaucrat.run_name) as reporter \
+	:
 		threads = []
 		for slot_number in set(voltages):
 			thread = MeasureIVCurveThread(
@@ -150,6 +162,7 @@ def measure_iv_curves_on_multiple_slots(bureaucrat:RunBureaucrat, voltages:dict,
 				n_measurements_per_voltage = n_measurements_per_voltage,
 				current_compliance = current_compliances[slot_number],
 				silent = silent,
+				reporter = reporter,
 			)
 			threads.append(thread)
 	
