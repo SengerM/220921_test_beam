c5d0fcba25dcda0a1f23b7976981c3965a52ce1e
Matias Senger <m.senger@hotmail.com>
2022-09-25 11:00:18 +0200

    Added `interlace`

--- utils.py
+++ utils.py
@@ -4,3 +4,15 @@ import time
 def create_a_timestamp():
 	time.sleep(1) # This is to ensure that no two timestamps are the same.
 	return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+def interlace(lst):
+	# https://en.wikipedia.org/wiki/Interlacing_(bitmaps)
+	lst = sorted(lst)[::-1]
+	result = [lst[0], lst[-1]]
+	ranges = [(1, len(lst) - 1)]
+	for start, stop in ranges:
+		if start < stop:
+			middle = (start + stop) // 2
+			result.append(lst[middle])
+			ranges += (start, middle), (middle + 1, stop)
+	return result
